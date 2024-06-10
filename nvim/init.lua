224c1a3c94b707923b6d8953b6eb8e4cd7abcad0
Revaz Nakhutsrishvili <r@revaz.dev>
2024-06-10 12:18:58 +0400

    feat(nvim): treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,4 @@ vim.g.maplocalleader = " "
 require "module.plugins"
 
 require "module.telescope"
+require "module.treesitter"
---
+++ nvim/lua/module/treesitter.lua
@@ -0,0 +1,21 @@
+local treesitter = require "nvim-treesitter.configs"
+
+treesitter.setup{
+  ensure_installed = {
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "javascript",
+    "typescript",
+    "json",
+    "python"
+  },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
