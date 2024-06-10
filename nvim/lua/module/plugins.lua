0c9bde3a13ac683977e26238cb1dc506a43d1118
Revaz Nakhutsrishvili <r@revaz.dev>
2024-06-10 11:37:41 +0400

    feat(nvim): install plugins
    vim-256noir(theme), plenary, telescope, treesitter, lsp, cmp

--- nvim/lua/module/plugins.lua
+++ nvim/lua/module/plugins.lua
@@ -11,7 +11,42 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-plugins = {}
+plugins = {
+  {
+    "andreasvc/vim-256noir",
+    config = function()
+      vim.o.termguicolors = true
+      vim.cmd.colorscheme "256_noir"
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim", tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "nvim-treesitter/nvim-treesitter-refactor",
+      "nvim-treesitter/playground",
+    },
+  },
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "VonHeikemen/lsp-zero.nvim",
+  "neovim/nvim-lspconfig",
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-cmdline",
+    },
+  },
+}
 opts = {}
 
 require"lazy".setup(plugins, opts)
