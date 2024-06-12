2f2896e62519dab9387777f0333bd16b972fca5f
Revaz Nakhutsrishvili <r@revaz.dev>
2024-06-13 03:16:34 +0400

    feat(nvim): cmp-path and cmp-buffer

--- nvim/lua/module/lsp.lua
+++ nvim/lua/module/lsp.lua
@@ -4,10 +4,46 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
+local cmp = require "cmp"
+
+cmp.setup {
+  mapping = cmp.mapping.preset.insert({
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+  }, {
+    { name = "buffer" },
+  })
+}
+
+cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" }
+  }
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" }
+  }, {
+    { name = "cmdline" }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
 local lspconfig = require "lspconfig"
 local mason = require "mason"
 mason.setup {}
 
+local cmp_nvim = require "cmp_nvim_lsp"
 local mason_lspconfig = require "mason-lspconfig"
 mason_lspconfig.setup {
   ensure_installed = {
@@ -23,20 +59,9 @@ mason_lspconfig.setup {
   },
   handlers = {
     function(server_name)
-      lspconfig[server_name].setup {}
+      lspconfig[server_name].setup {
+        capabilities = cmp_nvim.default_capabilities()
+      }
     end,
   }
-}
-
-local cmp = require "cmp"
-
-cmp.setup {
-  mapping = cmp.mapping.preset.insert({
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-  }),
 }
